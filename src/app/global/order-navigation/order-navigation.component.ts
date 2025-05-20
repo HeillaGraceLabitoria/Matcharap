45c93751a3cf5ceaec711eb0f32a8b70fa23eb3a
heilla_grace <labitoriaheilla1@gmail.com>
2025-05-20 15:08:19 +0800

    add seaarch

--- src/app/global/order-navigation/order-navigation.component.ts
+++ src/app/global/order-navigation/order-navigation.component.ts
@@ -1,26 +1,46 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Router } from '@angular/router';
+import products from '../../../data/products.json';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-order-navigation',
   standalone: true,  // if you're using Angular standalone components
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './order-navigation.component.html',
   styleUrls: ['./order-navigation.component.scss']  // fixed plural
 })
 export class OrderNavigationComponent {
-  @Input() cart: any[] = [];  // receive cart as input
+  searchQuery: string = '';
+  filteredProducts: any[] = [];
+  cart: any[] = [];
 
   constructor(private router: Router) {}
-  getCartCount(): number {
-    return this.cart.reduce((total, item) => total + (item.quantityAdded || 1), 0);
+
+  onSearch() {
+    const query = this.searchQuery.toLowerCase();
+    this.filteredProducts = query
+      ? products.filter(product => product.title.toLowerCase().includes(query))
+      : [];
+  }
+
+  goToMenuWithSearch(product: any) {
+    localStorage.setItem('searchProduct', JSON.stringify(product));
+    this.router.navigate(['/menu']);
+    this.searchQuery = '';
+    this.filteredProducts = [];
+  }
+
+  goToCart() {
+    this.router.navigate(['/order']);
   }
 
   goToCustomer() {
-    this.router.navigate(['/customer']);
+    this.router.navigate(['/user']);
   }
-  goToCart() {
-    this.router.navigate(['/order']);
+
+  getCartCount() {
+    return this.cart.reduce((acc, item) => acc + (item.quantity || 1), 0);
   }
 }
